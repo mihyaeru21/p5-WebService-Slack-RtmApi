bf6f205a91ea831c05d8978b2bad7e50f2f62b3b
Mihyaeru <mihyaeru21@gmail.com>
2015-05-09 20:47:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::Slack::RtmApi - It's new $module
+
+# SYNOPSIS
+
+    use WebService::Slack::RtmApi;
+
+# DESCRIPTION
+
+WebService::Slack::RtmApi is ...
+
+# LICENSE
+
+Copyright (C) Mihyaeru.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Mihyaeru <mihyaeru21@gmail.com>
---
+++ lib/WebService/Slack/RtmApi.pm
@@ -0,0 +1,39 @@
+package WebService::Slack::RtmApi;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::Slack::RtmApi - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::Slack::RtmApi;
+
+=head1 DESCRIPTION
+
+WebService::Slack::RtmApi is ...
+
+=head1 LICENSE
+
+Copyright (C) Mihyaeru.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Mihyaeru E<lt>mihyaeru21@gmail.comE<gt>
+
+=cut
+
